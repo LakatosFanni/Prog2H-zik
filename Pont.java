ba1a8548e0b621e319f0043dfba7c4b121231084
LakatosFanni <43538352+LakatosFanni@users.noreply.github.com>
2018-09-29 20:59:42 +0200

    Add files via upload

---
+++ Pont.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fannu
+ */
+public class Pont {
+  private double k1;
+  private double k2;
+    
+   public Pont (double k1, double k2){ 
+       
+       this.k1=k1;
+       this.k2=k2;
+    }
+   
+
+    public double getK1() {
+        return k1;
+    }
+
+    public void setK1(double k1) {
+        this.k1 = k1;
+    } 
+    
+    public double getK2 () {
+        return k2;
+    } 
+   
+    public void setK2(double k2){
+        this.k2 = k2;
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fannu
+ */
+public class main {
+public static void main(String[] args) {
+//        Pont p1=new Pont(2, 5);
+//        Pont p2=new Pont(1, 3);
+//        Pont p3=new Pont(4, 4);
+//        Pont p4=new Pont(6, 12);
+//        
+//        p1.setK2(p1.getK2()+5);
+//        p2.setK2(p2.getK2()+5);
+//        p3.setK1(p3.getK1()-3.4);
+//        p4.setK1(p4.getK1()-3.4);
+//       
+//        System.out.println(p1.getK2());
+//        System.out.println(p2.getK2());
+//        System.out.println(p3.getK1());
+//        System.out.println(p4.getK1());
+        
+//        RegularPolygon r1 = new RegularPolygon();
+//        RegularPolygon r2 = new RegularPolygon(6, 4);
+//        RegularPolygon r3 = new RegularPolygon(10, 4, 5.6, 7.8);
+//        
+//        System.out.println("Az r1 kerülete: " +r1.getPerimeter());
+//        System.out.println("Az r1 terulete: " +r1.getArea());
+//        System.out.println("Az r2 kerülete: " +r2.getPerimeter());
+//        System.out.println("Az r2 terulete: " +r2.getArea());
+//        System.out.println("Az r3 kerülete: " +r3.getPerimeter());
+//        System.out.println("Az r3 terulete: " +r3.getArea());
+
+//     Rectangle r1 = new Rectangle(4, 40);
+//     Rectangle r2 = new Rectangle(3.5, 35.9);
+//     
+//     System.out.println("r1 szélessége: "+r1.width);
+//     System.out.println("r1 magassága: "+r1.height);
+//     System.out.println("r1 területe: "+r1.getArea());
+//     System.out.println("r1 kerülete: "+r1.getPerimeter());
+//     
+//     System.out.println("r2 szélessége: "+r2.width);
+//     System.out.println("r2 magassága: "+r2.height);
+//     System.out.println("r2 területe: "+r2.getArea());
+//     System.out.println("r2 kerülete: "+r2.getPerimeter());
+//       
+//         
+  
+//        Ital i1 = new Ital();
+//        Ital i2 = new Ital();
+//        
+//        if(i1.equals(i2)){
+//            System.out.println("A két termék megegyezik");
+//        }
+  
+
+//        
+//        
+//QuadraticEquation e1= new QuadraticEquation(1, 2, 3);
+//QuadraticEquation e2= new QuadraticEquation(2, 4, 1);
+//QuadraticEquation e3 = new QuadraticEquation(-5, 1, -3);
+//
+// System.out.println("x1="+e1.getRoot1()+" "+"x2="+e1.getRoot2());
+// System.out.println("x1="+e2.getRoot1()+" "+ "x2="+e2.getRoot2());
+// System.out.println("x1="+e3.getRoot1()+" "+ "x2=" +e3.getRoot2());
+ 
+LinearEquation l1 = new LinearEquation (1, 2, 3, 8, 7, 5);
+LinearEquation l2 = new LinearEquation(4, 5, -6, 3, 2, 1);
+LinearEquation l3 = new LinearEquation(-1, 2, 3, -4, -7, 8);
+        
+        System.out.println("l1 értékei: " + l1.getX() + ", " + l1.getY());
+        System.out.println("l2 érékei: " + l2.getX() + ", " + l2.getY());
+        System.out.println("l3 értékei: " + l3.getX() +", " + l3.getY());
+    } 
+    }
+
+ 
+

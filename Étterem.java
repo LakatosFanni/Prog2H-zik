031af7cd62304e6089dc0ff5c645a901aebb27de
LakatosFanni <43538352+LakatosFanni@users.noreply.github.com>
2018-10-10 19:50:48 +0200

    Add files via upload

---
+++ Étterem.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secondhw;
+
+/**
+ *
+ * @author Fannu
+ */
+public class Étterem extends VendéglátóipariEgység {
+    private String[] étlap;
+    private boolean dohányzó = false;
+     
+    public Étterem(String név, int férőhelyekSzáma, String[] étlap){
+        super(név, férőhelyekSzáma, false);
+        this.étlap= étlap;
+    }
+
+    public String[] getÉtlap() {
+        return étlap;
+    }
+
+    @Override
+    public String toString() {
+        return "\u00c9tterem{" + "\u00e9tlap=" + étlap + ", doh\u00e1nyz\u00f3=" + dohányzó + '}';
+    }
+
+   
+    
+    
+}
